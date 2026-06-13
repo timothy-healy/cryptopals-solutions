17171299209b96a3f160a40919717afaf02a56c8
timothy-healy <timhealyfive@gmail.com>
2026-06-13 10:45:18 -0700

    Added Challenge 11

---
+++ set2/Challenge11.py
@@ -0,0 +1,28 @@
+"""
+Cryptopals Set 2, Challenge 11; ECB/CBC detection oracle.
+
+Write a function that randomly encrypts plaintext in either ECB or CBC mode.
+The key used is randomly generated and unknown.
+Detect the block cipher mode the function is using each time.
+"""
+import sys
+sys.path.append("..")
+from utils import random_encrypt_aes, detect_ecb_cbc
+
+# Using 43 of the same byte consecutively guarantees second and third block are identical
+# Even after the random prepending/appending
+# Allows for ECB to be easily detected
+chosen_plaintext = b"a" * 43
+ciphertext, actual_mode = random_encrypt_aes(chosen_plaintext)
+detected = detect_ecb_cbc(ciphertext)
+print(f"Detected: {detected}")
+print(f"Correct: {detected == actual_mode}")
+
+# Do many tests to ensure not getting lucky
+print("\n10,000 tests:")
+for i in range(10000):
+    ciphertext, actual_mode = random_encrypt_aes(chosen_plaintext)
+    detected = detect_ecb_cbc(ciphertext)
+    if detected != actual_mode:
+        print("Incorrect")
+print("Done")
